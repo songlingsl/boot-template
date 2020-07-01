cd8f526306b9213f5138c2c7271c659f28c2e850
songlingqq <songlingsl@163.com>
2020-07-01 13:42:35 +0800

    加

---
+++ admin/src/test/java/com/ControllerTests.java
@@ -0,0 +1,20 @@
+package com;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class ControllerTests {
+
+    @Test
+    public  void testConfig(){//测试可以
+        log.info("fsdafdf");
+
+
+    }
+}
